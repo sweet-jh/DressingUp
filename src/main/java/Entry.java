5e93b268bca37bf6023ac07c9d02d5182179dd46
hjing <hjing@thoughtworks.com>
2017-01-08 14:52:23 +0800

    Add Shoes class

--- src/main/java/Entry.java
+++ src/main/java/Entry.java
@@ -6,14 +6,17 @@ public class Entry {
     public static void main(String[] args) {
         People Lily;
         Jeans jeans;
+        Shoes shoes;
         Tshirt tshirt;
 
         Lily = new People("Lily");
         jeans = new Jeans();
+        shoes = new Shoes();
         tshirt = new Tshirt();
 
         jeans.dressing(Lily);
-        tshirt.dressing(jeans);
+        shoes.dressing(jeans);
+        tshirt.dressing(shoes);
 
         tshirt.show();
 
---
+++ src/main/java/Shoes.java
@@ -0,0 +1,10 @@
+/**
+ * Created by hjing on 1/7/17.
+ */
+public class Shoes extends Decorate {
+    @Override
+    public void show() {
+        System.out.println("Wearing Shoes.");
+        super.show();
+    }
+}
